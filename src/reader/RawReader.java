feba4fe8299eb0503f5b570d518c972b8b5ed3db
Nathanaël COURET <nathanael.couret@ensimag.grenoble-inp.fr>
2015-11-10 16:14:42 +0100

    dependencies

--- src/reader/RawReader.java
+++ src/reader/RawReader.java
@@ -5,8 +5,17 @@ package reader;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.BitSet;
+
+import com.github.jinahya.bit.io.BitInput;
+import com.github.jinahya.bit.io.BitOutput;
+import com.github.jinahya.bit.io.ByteOutput;
+import com.github.jinahya.bit.io.DelegatedBitInput;
+import com.github.jinahya.bit.io.DelegatedBitOutput;
+import com.github.jinahya.bit.io.StreamInput;
+import com.github.jinahya.bit.io.StreamOutput;
 
 /**
  * @author Ataww
@@ -18,16 +27,21 @@ public class RawReader {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		try {
-			RawReader r = new RawReader("Lena.raw");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public static void main(String[] args) throws IOException {
+		ByteOutput bo = new StreamOutput(new FileOutputStream("test.test"));
+		BitOutput bio = new DelegatedBitOutput(bo);
+		for(int i = 0; i < 5; i++) {
+			bio.writeBoolean(i % 2 == 0);
 		}
-
+		BitInput bii = new DelegatedBitInput(new StreamInput(new FileInputStream("test.test")));
+		for(int i = 0; i < 5; i++) {
+			System.out.println(bii.readBoolean());
+		}
 	}
+	
+	
 
 	public RawReader(String path) throws IOException {
 		FileInputStream fis = new FileInputStream(path);
