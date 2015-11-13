7af8d1091404b67cc035b5ca9b67e004fada7552
ncouret <couretn@ensimag.grenoble-inp.fr>
2015-11-13 01:01:27 +0100

    application complete

--- src/compresseur/App.java
+++ src/compresseur/App.java
@@ -1,11 +1,14 @@
 package compresseur;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
 import reader.RawReader;
 import util.ImgMultiplier;
 import util.RawWriter;
+import util.SegReader;
 import util.SegWriter;
 
 public class App {
@@ -14,24 +17,24 @@ public class App {
 
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		if (args.length < 2) {
 			throw new IllegalArgumentException("not enough arguments, expecting mode and image's path");
-		} else if (!(args[0].equals("-r") || args[0].equals("-i") || args.equals("-g"))) {
-			throw new IllegalArgumentException("invalid mode, -i or -r expected");
+		} else if (!(args[0].equals("-r") || args[0].equals("-i") || args[0].equals("-g") || args[0].equals("-d"))) {
+			throw new IllegalArgumentException("invalid mode, -i, -r, -d or -g expected");
 		}
 		if (!new File(args[1]).exists()) {
 			throw new IllegalArgumentException("invalid image path, does not exists");
 		}
-		if (args[0].equals("-g")) {
+		if (args[0].equals("-g")) { // agrandir l'image
 			try {
 				RawReader reader = new RawReader(args[1]);
 				reader.read(false);
 				ImgMultiplier mult = new ImgMultiplier(reader.getData());
 				byte[] output = mult.grow();
-				String[] fileSegments = args[1].split(".");
+				String[] fileSegments = args[1].split("\\.");
 				StringBuilder sb = new StringBuilder(fileSegments[0]);
-				for (int i = 1; i < fileSegments.length - 1; i++) {
+				for (int i = 1; i < fileSegments.length - 2; i++) {
 					sb.append("." + fileSegments[i]);
 				}
 				sb.append("_g.");
@@ -41,20 +44,38 @@ public class App {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		} else {
+		}else if(args[0].equals("-d")){ // décompresser l'image
+			String[] fileSegments = args[1].split("\\.");
+			if(!fileSegments[fileSegments.length-1].equals("seg")) {
+				throw new IllegalArgumentException("seg file expected for decompression");
+			}
+			StringBuilder sb = new StringBuilder(fileSegments[0]);
+			for(int i = 1; i < fileSegments.length - 2; i++) {
+				sb.append("."+fileSegments[i]);
+			}
+			sb.append(".raw");
+			SegReader sr = new SegReader(args[1]);
+			List<Pixel> pxls = sr.read();
+			RawWriter rw = new RawWriter(sb.toString());
+			rw.write(pxls);
+		}else { // compression
 			try {
 				RawReader rawReader = new RawReader(args[1]);
 				rawReader.read(true);
 				Rouleau compresseur = new Rouleau(rawReader.buildList());
 				compresseur.compresseur(args[0].equals("-i"), false);
-				SegWriter sw = new SegWriter(args[1].split(".")[0] + ".seg", rawReader.getData());
+				String[] fileSegments = args[1].split("\\.");
+				StringBuilder sb = new StringBuilder(fileSegments[0]);
+				for(int i = 1; i < fileSegments.length - 2; i++) {
+					sb.append("."+fileSegments[i]);
+				}
+				sb.append(".seg");
+				SegWriter sw = new SegWriter(sb.toString(), rawReader.getData());
 				sw.write(compresseur.getChemin());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-
-
 
 }
